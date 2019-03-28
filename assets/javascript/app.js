3f7b9d4f55eb581e20dc160e810477638313f670
Zach Drake <zadrake@yahoo.com>
2019-03-27 20:09:59 -0700

    weird table data issues

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,36 +11,61 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-//  updating browser from firebase
-//   database.ref.on("child_added", function (childsnapshot){
+database.ref().on("child_added", function (childSnapshot) {
+    var children = [
+        name = childSnapshot.val().trainName,
+        destination = childSnapshot.val().destination,
+        frequency = childSnapshot.val().frequency,
+        arrival = childSnapshot.val().nextArrival,
+        mins = childSnapshot.val().minsAway
+    ]
 
-//   });
+    var newRow = $('<tr>');
+    newRow.attr('class', "table-row");
+    $('#table').append(newRow);
+    for (var i = 0; i < children.length; i++) {
+        var newCol = $('<td>');
+        newCol.text(children[i]);
+        $('.table-row').append(newCol);
+    }
+
+});
+
 
 $('button').on('click', function (event) {
     event.preventDefault();
 
+    var frequency = parseInt($('#frequency').val().trim());
+    var trainTime = $('#train-time').val().trim();
+    var firstTimeConverted = moment(trainTime, "HH:mm");
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    var tRemainder = diffTime % frequency;
+    var minsAway = frequency - tRemainder;
+    var nextArrival = moment().add(minsAway, "minutes").format("hh:mm");
     var colList = [
-         trainName = $('#train-name').val().trim() ,
-         destination = $('#destination').val().trim(),
-         trainTime = parseInt($('#train-time').val().trim()),
-         frequency = parseInt($('#frequency').val().trim())
-
+        trainName = $('#train-name').val().trim(),
+        destination = $('#destination').val().trim(),
+        frequency,
+        nextArrival,
+        minsAway
     ]
-    // var trainName = $('#train-name').val().trim();
-    // var destination = $('#destination').val().trim();
-    // var trainTime = parseInt($('#train-time').val().trim());
-    // var frequency = parseInt($('#frequency').val().trim());
-    console.log(colList[i]);
-    var newRow = $('<tr>');
-    newRow.attr('class', "table-row");
-    
-    $('#table').append(newRow);
-    for (var i = 0; i < colList.length; i++) {
-        var newCol = $('<td>');
-        newCol.text(colList[i]);
-        $('.table-row').append(newCol);
 
-    }
+    // var newRow = $('<tr>');
+    // newRow.attr('class', "table-row");
+    // $('#table').append(newRow);
+    // for (var i = 0; i < colList.length; i++) {
+    //     var newCol = $('<td>');
+    //     newCol.text(colList[i]);
+    //     $('.table-row').append(newCol);
+    // }
 
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        frequency: frequency,
+        nextArrival: nextArrival,
+        minsAway: minsAway
 
+    })
+    $('form :input').val('');
 });
